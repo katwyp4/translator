de366fa85f2315637ab0bbfda54135225ee96f01
245965 <245965@edu.p.lodz.pl>
2024-03-20 16:48:02 +0100

    Creating declaration of enum error_t , struct word_t, function : read_words,delete_words,translate_words

--- translator.h
+++ translator.h
@@ -1,8 +1,21 @@
-//
-// Created by kasia on 20.03.2024.
-//
+#ifndef TRANSLATOR_H
+#define TRANSLATOR_H
 
-#ifndef TRANSLATOR_TRANSLATOR_H
-#define TRANSLATOR_TRANSLATOR_H
+enum error_t{
+    ERROR_OK,
+    ERROR_INCORRECT_PARAMETERS,
+    ERROR_FILE_IO,
+    ERROR_MEMORY
+};
 
-#endif //TRANSLATOR_TRANSLATOR_H
+struct word_t {
+    char* text_pl;
+    char* text_en;
+};
+
+struct word_t** read_words(const char* filename, enum error_t* errcode);
+void delete_words(struct word_t** tab);
+
+char** translate_words( struct word_t** tab, int n, ...);
+
+#endif

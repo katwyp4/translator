9193587a47ed923e161920e78eb59de16a4f7f87
245965 <245965@edu.p.lodz.pl>
2024-03-20 16:56:46 +0100

    Creating functions count_lines,allocate_memory,read_file,read_words

--- translator.c
+++ translator.c
@@ -1,3 +1,91 @@
-//
-// Created by kasia on 20.03.2024.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include "translator.h"
+
+int count_lines(FILE *file) {
+    int lines = 0;
+    int c;
+    while ((c = fgetc(file)) != EOF) {
+        if (c == '\n') {
+            lines++;
+        }
+    }
+    return lines;
+}
+
+struct word_t** allocate_memory(int count) {
+    struct word_t** words = malloc(sizeof(struct word_t*) * (count + 1));
+    if (words == NULL) {
+        return NULL;
+    }
+    *(words + count) = NULL;
+    return words;
+}
+
+int read_file(const char* filename, struct word_t** words, int count) {
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        return 0;
+    }
+
+    char buffer[100];
+    for (int i = 0; i < count; i++) {
+        *(words + i) = malloc(sizeof(struct word_t));
+        if (*(words + i) == NULL) {
+            fclose(file);
+            return 0;
+        }
+        if (fgets(buffer, sizeof(buffer), file) == NULL) {
+            fclose(file);
+            return 0;
+        }
+        char* txt1 = strtok(buffer, " ");
+        char* txt2 = strtok(NULL, "\n");
+        (*(words + i))->text_pl = strdup(txt1);
+        (*(words + i))->text_en = strdup(txt2);
+        if ((*(words + i))->text_pl == NULL || (*(words + i))->text_en == NULL) {
+            fclose(file);
+            return 0;
+        }
+    }
+    fclose(file);
+    return 1;
+}
+
+struct word_t** read_words(const char* filename, enum error_t* errcode) {
+    if (filename == NULL || errcode == NULL) {
+        if (errcode != NULL) {
+            *errcode = ERROR_INCORRECT_PARAMETERS;
+        }
+        return NULL;
+    }
+
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        if (errcode != NULL) {
+            *errcode = ERROR_FILE_IO;
+        }
+        return NULL;
+    }
+
+    int lines = count_lines(file);
+    fclose(file);
+
+    struct word_t** words = allocate_memory(lines);
+    if (words == NULL) {
+        *errcode = ERROR_MEMORY;
+        return NULL;
+    }
+
+    int success = read_file(filename, words, lines);
+    if (!success) {
+        delete_words(words);
+        *errcode = ERROR_MEMORY;
+        return NULL;
+    }
+
+    *errcode = ERROR_OK;
+    return words;
+}
